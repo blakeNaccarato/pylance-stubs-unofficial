2f09707c43fcb95cdf0d5488b68afb60872e87ac
blakeNaccarato <blake.naccarato@gmail.com>
2022-11-16 17:54:42 -0800

    Update script

--- update.ps1
+++ update.ps1
@@ -2,16 +2,23 @@ function Update-PylanceStubs {
     <#.SYNOPSIS
     Update stubs in pylance-stubs-unofficial with the latest from Pylance.
     #>
+
+    # Remove existing stubs
+    Get-ChildItem -Exclude ('README.md', 'update.ps1') | Remove-Item -Recurse
+
+    # Get the latest release
     $__ = @{
         Path   = '~/.vscode/extensions'
         Filter = 'ms-python.vscode-pylance*'
     }
     $latest_pylance = (Get-ChildItem @__)[-1]
-    $version = (($latest_pylance | Split-Path -Leaf) -Split '-')[-1]
+
+    # Copy the stubs
     $source = "$latest_pylance/dist/bundled/stubs"
-    $destination = '~/Code/mine/pylance-stubs-unofficial'
-    Get-ChildItem $destination -Exclude README.md, update.ps1 | Remove-Item -Recurse
     Get-ChildItem $source | Copy-Item -Recurse -Destination $destination
+
+    # Commit, tag, and push
+    $version = (($latest_pylance | Split-Path -Leaf) -Split '-')[-1]
     git add -A
     git commit -m $version
     git tag $version
